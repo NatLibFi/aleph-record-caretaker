fbdb6a497e16d9be8741302704e7f545ba9e33eb
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-06-06 17:16:13 +0300

    Adds serial fn for running promises in sequence

--- utils.js
+++ utils.js
@@ -1,16 +1,23 @@
-/* eslint-disable no-console */
+const _ = require('lodash');
 
 function decorateConnectionWithDebug(connection) {
 
   const actualExecute = connection.execute;
   connection.execute = function() {
-    console.log('DEBUG-SQL', `'${arguments[0]}'`, arguments[1]);
+    console.log('DEBUG-SQL', `'${arguments[0]}'`, arguments[1]); //eslint-disable-line no-console
     return actualExecute.apply(this, arguments);
   };
 }
 
+function serial(funcs) {
+  return funcs.reduce((promise, func) => {
+    return promise.then((all) => func().then(result => _.concat(all, result)));
+  }, Promise.resolve([]));
+}
+
 module.exports = {
-  decorateConnectionWithDebug
+  decorateConnectionWithDebug,
+  serial
 };
 
 
