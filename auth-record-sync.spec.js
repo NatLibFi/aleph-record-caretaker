93ed3732741022ff7ae9babe3fef473985bf598a
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-06-09 12:05:57 +0300

    Adds failing tests

--- auth-record-sync.spec.js
+++ auth-record-sync.spec.js
@@ -41,6 +41,11 @@ describe('AuthRecordSyncService', () => {
 
   describe('for linked bib records', () => {
 
+    beforeEach(() => {
+      recordServiceStub.loadRecord.withArgs('TST01', '00001').resolves(fakeBibRecord);
+      recordServiceStub.loadRecord.withArgs('TST10', '90001').resolves(fakeAuthRecord);
+      findServiceStub.findLinkedAgentRecords.withArgs('TST01', '90001').resolves(['00001']);
+    });
     
     it('should not save the record if it has not changed', async () => {
 
@@ -51,10 +56,6 @@ describe('AuthRecordSyncService', () => {
 
       fakeAuthRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡tcontent'));
       fakeBibRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡tcontent'));
-      
-      recordServiceStub.loadRecord.withArgs('TST10', '90001').resolves(fakeAuthRecord);
-      recordServiceStub.loadRecord.withArgs('TST01', '00001').resolves(fakeBibRecord);
-      findServiceStub.findLinkedAgentRecords.withArgs('TST01', '90001').resolves(['00001']);
       
       await recordSyncService.handleAuthChange(fakeChange);
 
@@ -73,10 +74,6 @@ describe('AuthRecordSyncService', () => {
       fakeBibRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, I,‡tcontent‡0(TST10)90001'));
       fakeBibRecord.appendField(RecordUtils.stringToField('700    ‡aAakkula, I,‡tcontent‡0(TST10)90001'));
       fakeAuthRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡tcontent'));
-
-      recordServiceStub.loadRecord.withArgs('TST01', '00001').resolves(fakeBibRecord);
-      recordServiceStub.loadRecord.withArgs('TST10', '90001').resolves(fakeAuthRecord);
-      findServiceStub.findLinkedAgentRecords.withArgs('TST01', '90001').resolves(['00001']);
 
       await recordSyncService.handleAuthChange(fakeChange);
 
@@ -101,10 +98,6 @@ describe('AuthRecordSyncService', () => {
       fakeBibRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, I,‡tcontent‡0(FARAWAY)999‡0(TST10)90001'));
       fakeAuthRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡tcontent'));
 
-      recordServiceStub.loadRecord.withArgs('TST01', '00001').resolves(fakeBibRecord);
-      recordServiceStub.loadRecord.withArgs('TST10', '90001').resolves(fakeAuthRecord);
-      findServiceStub.findLinkedAgentRecords.withArgs('TST01', '90001').resolves(['00001']);
-
       await recordSyncService.handleAuthChange(fakeChange);
 
       expect(recordServiceStub.saveRecord.callCount).to.equal(1, 'SaveRecord was not called');
@@ -128,10 +121,6 @@ describe('AuthRecordSyncService', () => {
       fakeAuthRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡tcontent'));
       fakeAuthRecord.appendField(RecordUtils.stringToField('STA    ‡aDELETED'));
 
-      recordServiceStub.loadRecord.withArgs('TST01', '00001').resolves(fakeBibRecord);
-      recordServiceStub.loadRecord.withArgs('TST10', '90001').resolves(fakeAuthRecord);
-      findServiceStub.findLinkedAgentRecords.withArgs('TST01', '90001').resolves(['00001']);
-
       await recordSyncService.handleAuthChange(fakeChange);
 
       expect(recordServiceStub.saveRecord.callCount).to.equal(0, 'SaveRecord was called for deleted record');
@@ -149,10 +138,6 @@ describe('AuthRecordSyncService', () => {
       fakeBibRecord.appendField(RecordUtils.stringToField('STA    ‡aDELETED'));
       fakeAuthRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡tcontent'));
       
-      recordServiceStub.loadRecord.withArgs('TST01', '00001').resolves(fakeBibRecord);
-      recordServiceStub.loadRecord.withArgs('TST10', '90001').resolves(fakeAuthRecord);
-      findServiceStub.findLinkedAgentRecords.withArgs('TST01', '90001').resolves(['00001']);
-
       await recordSyncService.handleAuthChange(fakeChange);
 
       expect(recordServiceStub.saveRecord.callCount).to.equal(0, 'SaveRecord was called for deleted record');
@@ -161,34 +146,52 @@ describe('AuthRecordSyncService', () => {
   });
 
   describe('for linked auth records', () => {
+    let fakeLinkedAuthRecord;
+    let fakeChange;
 
-    it('should copy the name from the authority record and update the linked record fields with it', async () => {
+    beforeEach(() => {
+      fakeLinkedAuthRecord = new MarcRecord(fakeAuthRecord);
 
-      const fakeChange = {
+      fakeChange = {
         recordId: '90001',
         library: 'TST10'
       };
-      const fakeLinkedAuthRecord = new MarcRecord(fakeAuthRecord);
-
-      fakeLinkedAuthRecord.appendField(RecordUtils.stringToField('400    ‡aAakkula, I,‡tcontent‡0(TST10)90001'));
-      fakeAuthRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡tcontent'));
 
       recordServiceStub.loadRecord.withArgs('TST10', '00001').resolves(fakeLinkedAuthRecord);
       recordServiceStub.loadRecord.withArgs('TST10', '90001').resolves(fakeAuthRecord);
       findServiceStub.findLinkedAgentRecords.withArgs('TST10', '90001').resolves(['00001']);
       findServiceStub.findLinkedAgentRecords.withArgs('TST01', '90001').resolves([]);
 
+    });
+
+    it('should copy the name from the authority record and update the linked record fields with it', async () => {
+
+      fakeAuthRecord.appendField(RecordUtils.stringToField('100    ‡aAakkula, Immo,‡d1934-1992‡0(TST10)90001'));
+      fakeLinkedAuthRecord.appendField(RecordUtils.stringToField('500    ‡aAakkula, I.,‡0(TST10)90001'));
+
       await recordSyncService.handleAuthChange(fakeChange);
 
       expect(recordServiceStub.saveRecord.callCount).to.equal(1, 'SaveRecord was not called');
-
-      const [base, recordId, record] = recordServiceStub.saveRecord.getCall(0).args;
-      expect(base).to.equal('TST10');
-      expect(recordId).to.equal('00001');
-
-      expect(RecordUtils.fieldToString(record.getFields('400')[0])).to.equal('400    ‡aAakkula, Immo,‡tcontent‡0(TST10)90001');
-
+      const [, , record] = recordServiceStub.saveRecord.getCall(0).args;
+ 
+      expect(RecordUtils.fieldToString(record.getFields('500')[0])).to.equal('500    ‡aAakkula, Immo,‡d1934-1992‡0(TST10)90001');
     });
+
+    it('should copy the name and tag if it changes', async () => {
+
+      fakeAuthRecord.appendField(RecordUtils.stringToField('110    ‡aAsia joka muuttui henkilöstä yhteisöksi‡0(TST10)90001'));
+      fakeLinkedAuthRecord.appendField(RecordUtils.stringToField('500    ‡aAsia joka on henkilö,‡cnimimerkki‡0(TST10)90001'));
+
+      await recordSyncService.handleAuthChange(fakeChange);
+
+      expect(recordServiceStub.saveRecord.callCount).to.equal(1, 'SaveRecord was not called');
+      const [, , record] = recordServiceStub.saveRecord.getCall(0).args;
+ 
+      expect(RecordUtils.fieldToString(record.getFields('510')[0])).to.equal('510    ‡aAsia joka muuttui henkilöstä yhteisöksi‡0(TST10)90001');
+      expect(RecordUtils.fieldToString(record.getFields('500'))).to.have.lengthOf(0);
+      
+    });
+
 
   });
 });
