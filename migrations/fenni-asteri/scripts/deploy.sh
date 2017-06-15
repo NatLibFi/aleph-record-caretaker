dda7ec6f4674402dd57f6cbacce15491b4cba3f5
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-06-15 17:03:45 +0300

    Cleanup

--- migrations/fenni-asteri/scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-
-
-rsync -avz --exclude node_modules --exclude tnsnames.ora --exclude dbconfig.js --delete .. petuomin@libnet2-kk.lib.helsinki.fi:/home/petuomin/sync-tool
-
-ssh libnet2-kk.lib.helsinki.fi -l petuomin -i ~/.ssh/id_dsa_kk -A -C "rsync -avz sync-tool petuomin@melinda-proc-kk.lib.helsinki.fi:/home/petuomin"
