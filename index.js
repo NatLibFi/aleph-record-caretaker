529165c4d9ba4e262a45095b18e0d3f385747bfe
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-08-09 13:29:05 +0300

    Adds ONLINE option
    Configure when the system polls aleph for changes.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const oracledb = require('oracledb');
 oracledb.outFormat = oracledb.OBJECT;
-const debug = require('debug')('auth-sync-service');
+const debug = require('debug')('main');
 const _ = require('lodash');
 
 const AlephChangeListener = require('aleph-change-listener');
@@ -24,6 +24,9 @@ const utils = require('./lib/utils');
 
 const DEBUG_SQL = process.env.DEBUG_SQL;
 
+const ONLINE = utils.readEnvironmentVariable('ONLINE', '00:00-21:55, 22:30-24:00');
+
+const onlineTimes = utils.parseTimeRanges(ONLINE);
 
 const NOOP = utils.readEnvironmentVariable('NOOP', '0');
 const noOperation = NOOP !== '0' ? true : false;
@@ -89,59 +92,94 @@ const alephFindService = AlephFindService.create(XServerUrl);
 const bibRecordSyncService = BibRecordSyncService.create(alephRecordService, alephFindService, bibSyncServiceOptions);
 const authRecordSyncService = AuthRecordSyncService.create(alephRecordService, alephFindService, authSyncServiceOptions);
 
-start().catch(error => { 
-  console.error(error); 
-  logger.log('error', error.message, error);
-});
+let alephChangeListener;
+let connection;
+
+logger.log('info', `Online times: ${ONLINE}. Current time: ${utils.getCurrentTime()}`);
+
+let isRunning = false;
+function updateOnlineState() {
+  const now = utils.parseTime(utils.getCurrentTime());
+  debug(`now is ${now}`);
+  if (onlineTimes.some(({from, to}) => from <= now && now <= to)) {
+    if (!isRunning) {
+            
+      start().catch(error => { 
+        console.error(error); 
+        logger.log('error', error.message, error);
+      });
+      isRunning = true;
+    }
+  } else {
+    if (isRunning) {
+            
+      stop().catch(error => { 
+        console.error(error); 
+        logger.log('error', error.message, error);
+      });
+      isRunning = false;
+    }
+  }
+}
+
+updateOnlineState();
+setInterval(updateOnlineState, 60000);
+
 
 async function start() {
   logger.log('info', 'Connecting to oracle');
-  const connection = await oracledb.getConnection(dbConfig);
+  connection = await oracledb.getConnection(dbConfig);
 
   if (DEBUG_SQL) {
     utils.decorateConnectionWithDebug(connection);
   }
 
   logger.log('info', 'Creating aleph changelistener');
-  const alephChangeListener = await AlephChangeListener.create(connection, options, onChange);
+  alephChangeListener = await AlephChangeListener.create(connection, options, onChange);
   
   logger.log('info', 'Starting aleph changelistener');
   alephChangeListener.start();
   
   logger.log('info', 'Waiting for changes');
+
 }
 
-function onChange(changes) {
-  logger.log('verbose', `Handling ${changes.length} changes.`);
+async function stop() {
+  if (alephChangeListener) {
+    alephChangeListener.stop();
+    logger.log('info', 'Stopped aleph changelistener');
+
+    await connection.close();
+    logger.log('info', 'Disconnected from oracle');
+  }
+}
+
+const noChangesLogger = utils.accumulate(12, () => {
+  logger.log('info', 'No changes.');
+});
+
+async function onChange(changes) {
+  debug(`Changes: ${changes.length}`);
+  if (changes.length === 0) {
+    return noChangesLogger();
+  }
+  logger.log('info', `Handling ${changes.length} changes.`);
+  noChangesLogger.reset();
   
-  return serial(changes.map((change) => () => {
-
-    switch(change.library) {
-      case 'FIN01': return bibRecordSyncService.handleBibChange(change);
-      //case 'FIN10': return authRecordSyncService.handleAuthChange(change);
-      case 'FIN11': return authRecordSyncService.handleAuthChange(change);
-      case 'FIN19': return authRecordSyncService.handleAuthChange(change);
-      default: return Promise.reject(new Error(`Could not find handler for base ${change.library}`));
+  for (const change of changes) {
+  
+    try {
+      switch(change.library) {
+        case 'FIN01': await bibRecordSyncService.handleBibChange(change); break;
+        case 'FIN11': await authRecordSyncService.handleAuthChange(change); break;
+        case 'FIN19': await authRecordSyncService.handleAuthChange(change); break;
+        default: logger.log('warn', `Could not find handler for base ${change.library}`); return;
+      }
+    } catch(error) {
+      logger.log('error', error.message, error);
+      console.error(error);
     }
-  })).catch(error => {
-    logger.log('error', error.message, error);
-    console.error(error);
-  });
-}
 
-function serial(funcs) {
-  return funcs.reduce((promise, func) => {
-    return new Promise((resolve) => {
-      promise.then((all) => {
-        func()
-          .then(result => resolve(_.concat(all, result)))
-          .catch(error => {
-            console.error(error);
-            logger.log('error', error.message, error);
-            resolve(_.concat(all, error));
-          });
-      });
-    });
-  }, Promise.resolve([]));
-}
+  }
 
+}
