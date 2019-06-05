82f00260837fa7340163f9797657b03d78ab24c8
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-06-05 14:56:45 +0300

    Update copyright year

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 University Of Helsinki (The National Library Of Finland)
+ * Copyright 2017-2019 University Of Helsinki (The National Library Of Finland)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
