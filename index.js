c84666fa483d05cc4bc4e8b043ef8e514d06bf49
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-08-12 09:37:33 +0300

    fix line work

--- index.js
+++ index.js
@@ -15,7 +15,6 @@
 */// running this requires at least node 7.10.0
 /* eslint no-console: 0 */
 
-
 const logger = require('./lib/logger');
 const fs = require('fs');
 const path = require('path');
@@ -48,7 +47,7 @@ async function run() {
   const CURSOR_FILE = utils.readEnvironmentVariable('CURSOR_FILE', '.aleph-changelistener-cursors.json');
   const Z106_STASH_PREFIX = utils.readEnvironmentVariable('Z106_STASH_PREFIX', '.z106_stash');
   const CHANGES_QUEUE_FILE = utils.readEnvironmentVariable('CHANGES_QUEUE_FILE', '.aleph-changelistener-changesqueue');
-  
+
   const DEBUG_SQL = process.env.DEBUG_SQL;
   const ONLINE = utils.readEnvironmentVariable('ONLINE', '00:00-21:55, 22:30-24:00');
   const onlineTimes = utils.parseTimeRanges(ONLINE);
@@ -56,20 +55,20 @@ async function run() {
   const noOperation = NOOP !== '0' ? true : false;
   const NOOP_BIBCHANGE = utils.readEnvironmentVariable('NOOP_BIBCHANGE', '0');
   const noOperationBibChange = (NOOP !== '0' || NOOP_BIBCHANGE !== '0') ? true : false;
-  
-  const {BibRules: bibRules, AuthRules: authRules} = Punctuation;  
-  
+
+  const {BibRules: bibRules, AuthRules: authRules} = Punctuation;
+
   const baseMap = {
     'FI-ASTERI-S': 'FIN10',
     'FI-ASTERI-N': 'FIN11'
   };
-  
+
   const urnBaseMap = {
     'FIN11': 'URN:NBN:fi:au:cn:'
   };
-  
+
   const urnResolverPrefix = 'http://urn.fi/';
-  
+
   const authSyncServiceOptions = {
     bibRecordBase: 'FIN01',
     agentRecordBase: 'FIN11',
@@ -81,7 +80,7 @@ async function run() {
     punctuationRulesForAuthRecord: authRules,
     punctuationRulesForBibRecord: bibRules
   };
-  
+
   const bibSyncServiceOptions = {
     noOperationBibChange,
     baseMap,
@@ -90,7 +89,7 @@ async function run() {
     logger,
     punctuationRulesForBibRecord: bibRules
   };
-  
+
   const options = {
     Z106Bases: Z106_BASES,
     Z115Base: Z115_BASE,
@@ -99,46 +98,45 @@ async function run() {
     Z106StashPrefix: Z106_STASH_PREFIX,
     changesQueueSaveFile: CHANGES_QUEUE_FILE
   };
-  
+
   const dbConfig = {
     user: utils.readEnvironmentVariable('ORACLE_USER'),
     password: utils.readEnvironmentVariable('ORACLE_PASS'),
     connectString: utils.readEnvironmentVariable('ORACLE_CONNECT_STRING')
   };
-  
+
   const XServerUrl = utils.readEnvironmentVariable('X_SERVER');
   const melindaEndpoint = utils.readEnvironmentVariable('MELINDA_API', 'http://libtest1.csc.fi:8992/API');
-  
+
   const credentials = {
     username: utils.readEnvironmentVariable('ALEPH_CARETAKER_USER'),
     password: utils.readEnvironmentVariable('ALEPH_CARETAKER_PASS')
   };
-  
+
   const alephRecordService = MelindaRecordService.createMelindaRecordService(melindaEndpoint, XServerUrl, credentials);
   const alephFindService = AlephFindService.create(XServerUrl);
-  
-  
+
+
   const bibRecordSyncService = BibRecordSyncService.create(alephRecordService, alephFindService, bibSyncServiceOptions);
   const authRecordSyncService = AuthRecordSyncService.create(alephRecordService, alephFindService, authSyncServiceOptions);
-  
+
   const noChangesLogger = utils.accumulate(12, () => {
     logger.log('info', 'No changes.');
   });
-  
+
   let alephChangeListener;
   let connection;
-  
+
   let isRunning = false;
-  
+
   oracledb.outFormat = oracledb.OBJECT;
-  
+
   logger.log('info', 'Starting aleph-record-caretaker');
   logger.log('info', `Online times: ${ONLINE}. Current time: ${utils.getCurrentTime()}`);
-  
-  
+
   await updateOnlineState();
   setInterval(updateOnlineState, 60000);
-  
+
   async function updateOnlineState() {
     const now = utils.parseTime(utils.getCurrentTime());
     debug(`now is ${now}`);
@@ -153,35 +151,35 @@ async function run() {
         isRunning = false;
       }
     }
-    
+
     async function start() {
       logger.log('info', 'Connecting to oracle');
       connection = await oracledb.getConnection(dbConfig);
-      
+
       if (DEBUG_SQL) {
         utils.decorateConnectionWithDebug(connection);
       }
-      
+
       logger.log('info', 'Creating aleph changelistener');
       alephChangeListener = await AlephChangeListener.create(connection, options, onChange);
-      
+
       logger.log('info', 'Starting aleph changelistener');
       alephChangeListener.start();
-      
+
       logger.log('info', 'Waiting for changes');
-      
+
     }
-    
+
     async function stop() {
       if (alephChangeListener) {
         alephChangeListener.stop();
         logger.log('info', 'Stopped aleph changelistener');
-        
+
         await connection.close();
         logger.log('info', 'Disconnected from oracle');
       }
     }
-    
+
     async function onChange(changes) {
       debug(`Changes: ${changes.length}`);
       if (changes.length === 0) {
@@ -189,8 +187,8 @@ async function run() {
       }
       logger.log('info', `Handling ${changes.length} changes.`);
       noChangesLogger.reset();
-      
-      for (const change of changes) {  
+
+      for (const change of changes) {
         try {
           switch(change.library) {
             case 'FIN01': await bibRecordSyncService.handleBibChange(change); break;
